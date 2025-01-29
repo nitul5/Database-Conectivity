e8edda2ebccbfc996e5009bc1894d428e49d8f83
Nitul Das <sziekcondearl123@gmail.com>
2025-01-30 02:30:16 +0530

    server_Database.js

--- server_Database.js
+++ server_Database.js
@@ -1,1 +1,51 @@
+// First we have install the mongodb module by using npm i mongodb 
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const mongodb = require('mongodb').MongoClient; 
+app.use(express.urlencoded({extended:true})); //Application level middleware
 
+//Defining the Database: 
+let database;
+let collection;
+let MyDatabase =  async()=>{
+    let connection = await mongodb.connect('mongodb://0.0.0.0:27017/');
+    database = await connection.db('LoginData');
+    collection = await database.createCollection('LoginDetails');
+}
+MyDatabase();
+
+// sending the login page: Note: Create your own form page with internal css!
+app.get('/login', async (req, res)=>{
+   res.writeHead(200, {"content-type":"text/html"});
+   let read = fs.createReadStream('./components/login.html');
+   read.pipe(res);
+});
+
+//Posting data to the respective database's collection!
+let datas;
+app.post('/login', async (req,res)=>{
+     datas = await req.body;
+    let insert = await collection.insertOne(datas);
+    let LoginData = `{_id:${datas._id} username: ${datas.username}, password: ${datas.password}}\n`;
+
+  //storing form data to a local file!
+    fs.appendFileSync('./loginDetails', LoginData);
+    res.send('Submitted');
+});
+
+//? displaying data in UI:
+app.get('/get-users', async (req, res) => {
+    try {
+        let users = await collection.find().toArray();
+        res.json(users);
+    } catch (err) {
+        res.status(500).send("Error fetching users");
+    }
+});
+
+// defining the port: 
+app.listen(5000, err=>{
+    if(err) throw err;
+    console.log("Hello EXpress");
+});
